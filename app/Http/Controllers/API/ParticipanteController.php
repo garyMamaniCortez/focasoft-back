bae22669c971d613748b2c0bc40d807015dbcdfc
garyMamaniCortez <mamanijhonny2210@gmail.com>
2023-10-13 12:25:52 -0400

    valida las preguntas obligatorias del formulario

--- app/Http/Controllers/API/ParticipanteController.php
+++ app/Http/Controllers/API/ParticipanteController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Participante;
+use App\Models\FormularioRegistro;
 
 class ParticipanteController extends Controller
 {
@@ -27,32 +29,19 @@ class ParticipanteController extends Controller
      */
     public function store(Request $request)
     {
-        $participante = new Participante;
-        $participante->nombres = $request->input('nombres');
-        $participante->apellidos = $request->input('apellidos');
-        $participante->fecha_nacimiento = $request->input('fecha_nacimiento');
-        $participante->correo_electronico = $request->input('correo_electronico');
-        $participante->numero_celular = $request->input('numero_celular');
-        $participante->carrera = $request->input('carrera');
-        $participante->talla_polera = $request->input('talla_polera');
-        $participante->carnet_identidad = $request->input('carnet_identidad');
-        $participante->codigo_sis_o_institucion = $request->input('codigo_sis_o_institucion');
-        $participante->semestre = $request->input('semestre');
-        $participante->id_formulario = $request->input('id_formulario');
-
-        if ($participante->nombres && $participante->apellidos && $participante->fecha_nacimiento && $participante->correo_electronico && $participante->numero_celular) {
-            try {
-                $participante->save();
-            } catch (\Exception $e) {
-                return response()->json(['error' => $e->getMessage()], 500);
-            }
-            return response()->json($participante,201);
-        } else {
+        if(!$this->isValidForm($request))
+        {
             return response()->json([
-                "error" => "Faltan campos obligatorios",
-                "campos_obligatorios" => ["nombres", "apellidos", "fecha_nacimiento", "correo_electronico", "numero_celular"]
+                "error" => "Debe llenar los campos obligatorios"
               ], 400);
         }
+        $participante = $this->createParticipante($request, null);
+        try {
+            $participante->save();
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+        return response()->json($participante,201);
     }
 
     /**
@@ -76,31 +65,19 @@ class ParticipanteController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $participante = Participante::findOrFail($id);
-        $participante->nombres = $request->input('nombres');
-        $participante->apellidos = $request->input('apellidos');
-        $participante->fecha_nacimiento = $request->input('fecha_nacimiento');
-        $participante->correo_electronico = $request->input('correo_electronico');
-        $participante->numero_celular = $request->input('numero_celular');
-        $participante->carrera = $request->input('carrera');
-        $participante->talla_polera = $request->input('talla_polera');
-        $participante->carnet_identidad = $request->input('carnet_identidad');
-        $participante->codigo_sis_o_institucion = $request->input('codigo_sis_o_institucion');
-        $participante->semestre = $request->input('semestre');
-
-        if ($participante->nombres && $participante->apellidos && $participante->fecha_nacimiento && $participante->correo_electronico && $participante->numero_celular) {
-            try {
-                $participante->save();
-            } catch (\Exception $e) {
-                return response()->json(['error' => $e->getMessage()], 500);
-            }
-            return response()->json($participante,200);
-        } else {
+        if(!$this->isValidForm($request))
+        {
             return response()->json([
-                "error" => "Faltan campos obligatorios",
-                "campos_obligatorios" => ["nombres", "apellidos", "fecha_nacimiento", "correo_electronico", "numero_celular"]
+                "error" => "Debe llenar los campos obligatorios"
               ], 400);
         }
+        $participante = $this->createParticipante($request, $id);
+        try {
+            $participante->save();
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+        return response()->json($participante,200);
     }
 
     /**
@@ -112,5 +89,50 @@ class ParticipanteController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    private function createParticipante(Request $request, $id)
+    {
+        $participante = new Participante;
+        if ($id != null)
+        {
+            $participante = Participante::findOrFail($id);
+        }
+        $participante->nombres = $request->input('nombres');
+        $participante->apellidos = $request->input('apellidos');
+        $participante->fecha_nacimiento = $request->input('fecha_nacimiento');
+        $participante->correo_electronico = $request->input('correo_electronico');
+        $participante->numero_celular = $request->input('numero_celular');
+        $participante->carrera = $request->input('carrera');
+        $participante->talla_polera = $request->input('talla_polera');
+        $participante->carnet_identidad = $request->input('carnet_identidad');
+        $participante->codigo_sis_o_institucion = $request->input('codigo_sis_o_institucion');
+        $participante->semestre = $request->input('semestre');
+        $participante->id_formulario = $request->input('id_formulario');
+
+        return $participante;
+    }
+
+    private function isValidForm(Request $request)
+    {
+        $formulario = FormularioRegistro::find($request->input('id_formulario'))->toArray();
+        $validator = Validator::make($request->all(), [
+            'nombres' => ($formulario['nombres'] == 1) ? 'required' : '',
+            'apellidos' => ($formulario['apellidos'] == 1) ? 'required' : '',
+            'fecha_nacimiento' => ($formulario['fecha_nacimiento'] == 1) ? 'required' : '',
+            'correo_electronico' => ($formulario['correo_electronico'] == 1) ? 'required|email' : '',
+            'numero_celular' => ($formulario['numero_celular'] == 1) ? 'required|numeric' : '',
+            'carrera' => ($formulario['carrera'] == 1) ? 'required' : '',
+            'talla_polera' => ($formulario['talla_polera'] == 1) ? 'required' : '',
+            'carnet_identidad' => ($formulario['carnet_identidad'] == 1) ? 'required' : '',
+            'codigo_sis_o_institucion' => ($formulario['codigo_sis_o_institucion'] == 1) ? 'required' : '',
+            'semestre' => ($formulario['semestre'] == 1) ? 'required' : '',
+            'id_formulario' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return false;
+        }
+        return true;
     }
 }
